d949e9f143d7de804e3b6708c30545d2113467f8
Tetja-na <tanjagrunertt@gmail.com>
2025-06-13 11:32:15 +0200

    commit2

---
+++ jsBasics/Hausaufgabe4.3.js
@@ -0,0 +1,9 @@
+//Завдання 4.3
+//    Створіть змінну radius і присвойте їй числове значення радіуса циліндра.
+   // Створіть змінну height і присвойте їй числове значення висоти циліндра.
+   // Обчисліть об'єм циліндра за формулою π * radius^2 * height і виведіть результат.
+//Округліть кожне отримане значення до 2 значень після крапки
+let radiusCylinder = 3;
+let height = 7;
+let cyllinderVolume = Math.PI * Math.pow(radiusCylinder, 2) * height;
+console.log("Zylindervolumen: " , cyllinderVolume.toFixed(2));
